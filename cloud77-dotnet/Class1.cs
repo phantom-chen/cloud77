6fcbc6e8969c33defe3c9cbd156b83193dcfda0c
Phantom.Chen <phantom0018@163.com>
2024-09-20 15:02:36 +0800

    add dotnet library and app

---
+++ cloud77-dotnet/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cloud77.Dotnet
+{
+    public class Class1
+    {
+        public int MyProperty { get; set; }
+
+        public string Greeting()
+        {
+            return "hello";
+        }
+    }
+}

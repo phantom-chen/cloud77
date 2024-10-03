d35d233513383382b88437f6a3fafc401fd3b014
Phantom.Chen <phantom0018@163.com>
2024-10-03 13:41:20 +0800

    add landing app

---
+++ landing-app/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];

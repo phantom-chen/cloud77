fa67a1ef0b79889cfe68362c8fb23ec6838ff48b
Phantom.Chen <phantom0018@163.com>
2024-10-05 20:03:24 +0800

    add simple desktop app

---
+++ Cooler.Plus/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Cooler.Plus.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cooler.Plus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new UpdateWindow();
+            window.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    var window = new UpdateWindow();
+                    window.ShowDialog();
+                });
+            });
+        }
+    }
+}

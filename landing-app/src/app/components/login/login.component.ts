0329afabe3f4c15e1df46dfef4d9b4220f2a4f15
Phantom.Chen <phantom0018@163.com>
2024-10-09 23:45:45 +0800

    update landing app

--- landing-app/src/app/components/login/login.component.ts
+++ landing-app/src/app/components/login/login.component.ts
@@ -1,11 +1,26 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { GatewayService } from '../../gateway.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCommonModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    MatCommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [
 
@@ -14,6 +29,10 @@ import { GatewayService } from '../../gateway.service';
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit, AfterViewInit {
+
+  remember: boolean = false;
+  account: string = '';
+  password: string = '';
   constructor(private service: GatewayService) {}
   ngAfterViewInit(): void {
 
@@ -23,9 +42,23 @@ export class LoginComponent implements OnInit, AfterViewInit {
     this.service.testing();
   }
 
+  onKeyUp(event: KeyboardEvent): void {
+    if (event.type === 'keyup' && event.key === 'Enter') {
+      this.login();
+    }
+  }
+
   login(): void {
     console.log('login works')
     this.service.testing();
     this.service.ping();
+  }
+
+  onAccountChange(): void {
+    console.log('account changed')
+  }
+
+  forgetPassword(): void {
+    console.log('forget password works')
   }
 }

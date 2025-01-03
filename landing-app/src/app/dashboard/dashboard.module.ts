23b35bdd1bb4d4d838679176d1439ba8d6d1ff35
Phantom.Chen <phantom0018@163.com>
2025-01-03 16:55:11 +0800

    update dashboard app

--- landing-app/src/app/dashboard/dashboard.module.ts
+++ landing-app/src/app/dashboard/dashboard.module.ts
@@ -3,15 +3,30 @@ import { CommonModule } from '@angular/common';
 import { provideRouter, Routes } from '@angular/router';
 import { HistoryComponent } from './history/history.component';
 import { SettingsComponent } from './settings/settings.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: StatisticsComponent
+  },
+  {
+    path: 'history',
     component: HistoryComponent
   },
   {
     path: 'settings',
     component: SettingsComponent
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent
+  },
+  {
+    path: 'accounts',
+    component: AccountsComponent
   }
 ];
 
--- landing-app/src/app/dashboard/statistics/statistics.component.ts
+++ landing-app/src/app/dashboard/statistics/statistics.component.ts
@@ -1,12 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgxChartsModule } from "@swimlane/ngx-charts";
 
 @Component({
   selector: 'app-statistics',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NgxChartsModule
+  ],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.css'
 })
 export class StatisticsComponent {
+  isLogin = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  view: [number, number] = [700, 400];
 
+  single = [
+    {
+      'name': 'Germany',
+      'value': 8940000
+    },
+    {
+      'name': 'USA',
+      'value': 5000000
+    },
+    {
+      'name': 'France',
+      'value': 7200000
+    },
+      {
+      'name': 'UK',
+      'value': 6200000
+    }
+  ];
+
+  single2 = [
+    {
+      'name': 'test1',
+      'value': 100
+    },
+    {
+      'name': 'test2',
+      'value': 200
+    },
+    {
+      'name': 'test3',
+      'value': 300
+    },
+      {
+      'name': 'test4',
+      'value': 400
+    }
+  ];
+
+  reload() {
+    // window.location.reload();
+    document.location.reload();
+  }
 }

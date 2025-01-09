6885c90a497051b65da18ff7aaa34ee3f8463f87
Phantom.Chen <phantom0018@163.com>
2025-01-09 14:20:58 +0800

    update icons

--- canteen-app/src/App.tsx
+++ canteen-app/src/App.tsx
@@ -43,25 +43,25 @@ function App() {
                     <img
                         className="UnderConstruction-social-networks-image"
                         alt="hello"
-                        src="assets/facebook_icon.png"></img>
+                        src="assets/facebook_16x16.png"></img>
                 </a>
                 <a className="UnderConstruction-social-networks-link">
                     <img
                         className="UnderConstruction-social-networks-image"
                         alt="hello"
-                        src="assets/facebook_icon.png"></img>
+                        src="assets/twitter_16x16.png"></img>
                 </a>
                 <a className="UnderConstruction-social-networks-link">
                     <img
                         className="UnderConstruction-social-networks-image"
                         alt="hello"
-                        src="assets/facebook_icon.png"></img>
+                        src="assets/linkedin_16x16.png"></img>
                 </a>
                 <a className="UnderConstruction-social-networks-link">
                     <img
                         className="UnderConstruction-social-networks-image"
                         alt="hello"
-                        src="assets/facebook_icon.png"></img>
+                        src="assets/emailto_16x16.png"></img>
                 </a>
             </div>
 
--- canteen-app/src/pages/Home.tsx
+++ canteen-app/src/pages/Home.tsx
@@ -5,6 +5,28 @@ const Home: React.FC = () => {
         <div>
             <h1>Welcome to the Canteen App</h1>
             <p>This is the home page.</p>
+            <button onClick={() => {
+                fetch('/hottopic/browse/topicList').then(res => {
+                    res.json().then(value => console.log(value));
+                })
+            }}>Topic List</button>
+
+            <button onClick={() => {
+                fetch('/json/users').then(res => {
+                    res.text().then(value => console.log(value));
+                });
+            }}>Json (users)</button>
+            <button onClick={() => {
+                fetch('/json/todos').then(res => {
+                    res.text().then(value => console.log(value));
+                });
+            }}>Json (todos)</button>
+
+            <button onClick={() => {
+                fetch('/resources/site.json').then(res => {
+                    res.text().then(value => console.log(value));
+                });
+            }}>Site</button>
         </div>
     );
 };

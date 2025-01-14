24cc8b3c5dbfde509f1d47851a738d0336b8eb1a
Phantom.Chen <phantom0018@163.com>
2025-01-14 19:32:42 +0800

    update login page

--- landing-app/src/app/pages/login/login.component.ts
+++ landing-app/src/app/pages/login/login.component.ts
@@ -2,13 +2,25 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCommonModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule,
+    MatCommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAutocompleteModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -18,28 +30,38 @@ export class LoginComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.email = sessionStorage.getItem('email') || '';
+    this.email = sessionStorage.getItem('user_email') || '';
   }
 
   email = '';
-
+  account: string = '';
+  
   password = '';
+
+  loginRequired: boolean = true;
+
+  users: string[] = ['user1@example.com','user2@example.com','user3@example.com'];
 
   onEmailChange(event: any) {
     console.log(event);
   }
 
   onLoginClick() {
-    console.log('login clicked');
-    console.log(this.email);
-    console.log(this.password);
-    sessionStorage.setItem('email', this.email);
+    if (this.account.includes('@')) {
+      sessionStorage.setItem('user_email', this.account);
+    }
 
-    this.http.post(`/api/users/token?email=${this.email}&password=${this.password}`, undefined)
+    this.http.post(`/user-app/users/token?email=${this.account}&password=${this.password}`, undefined)
     .subscribe((data: any) => {
       console.log(data);
-      sessionStorage.setItem('access-token', data.value);
-      sessionStorage.setItem('refresh-token', data.refreshToken);
+      sessionStorage.setItem('user_access_token', data.value);
+      sessionStorage.setItem('user_refresh_token', data.refreshToken);
     });
+  }
+
+  onKeyUp(event: KeyboardEvent): void {
+    if (event.type === 'keyup' && event.key === 'Enter') {
+      this.onLoginClick();
+    }
   }
 }

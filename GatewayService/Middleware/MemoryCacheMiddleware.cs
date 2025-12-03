e7d10620f4bc78b2bbd9ab454d11db6d0ca845a7
Phantom.Chen <phantom0018@163.com>
2025-12-03 16:20:48 +0800

    update gateway service

---
+++ GatewayService/Middleware/MemoryCacheMiddleware.cs
@@ -0,0 +1,58 @@
+﻿namespace GatewayService.Middleware
+{
+  public class MemoryCacheMiddleware
+  {
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger _logger;
+
+    private Dictionary<string, string> cache = new Dictionary<string, string>();
+
+    public MemoryCacheMiddleware(
+    RequestDelegate next,
+    IConfiguration configuration,
+    ILoggerFactory logFactory)
+    {
+      _next = next;
+      _configuration = configuration;
+      _logger = logFactory.CreateLogger<MemoryCacheMiddleware>();
+      _logger.LogInformation("cache middleware is running");
+    }
+
+    private string GetCache(string endpoint)
+    {
+      if (cache.ContainsKey(endpoint))
+      {
+        return cache[endpoint];
+      }
+      else
+      {
+        return null;
+      }
+    }
+
+    private void InsertCache(string endpoint, string payload)
+    {
+      cache.Add(endpoint, payload);
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+      var path = context.Request.Path.Value;
+      var cache = GetCache(path);
+      if (!string.IsNullOrEmpty(cache))
+      {
+        context.Response.StatusCode = 200;
+        context.Response.ContentType = "application/json";
+        context.Response.Headers.Append("X-Response-Data", "Middleware cache");
+        await context.Response.WriteAsync(cache);
+      }
+      else
+      {
+        await _next.Invoke(context);
+        //context.Response.Body.ToString();
+        //InsertCache(context.Request.Path.Value, Newtonsoft.Json.JsonConvert.SerializeObject(result));
+      }
+    }
+  }
+}
---
+++ GatewayService/Models/LocalDataModel.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace GatewayService.Models
+{
+  public class LocalDataModel
+  {
+    static LocalDataModel()
+    {
+      var location = Assembly.GetExecutingAssembly().Location;
+      var root = Directory.GetParent(location)?.ToString() ?? "";
+
+      if (!string.IsNullOrEmpty(root))
+      {
+        var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        if (isWindows)
+        {
+          string programDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+          Root = Path.Combine(programDataPath, "MyServices");
+        }
+        else
+        {
+          // for Linux system
+          Root = Path.Combine(root, "data");
+        }
+      }
+
+      CustomLogging = Environment.GetEnvironmentVariable("CUSTOM_LOGGING") ?? "";
+
+      if (!string.IsNullOrEmpty(root) && !Directory.Exists(Root))
+      {
+        Directory.CreateDirectory(Root);
+
+        if (!Directory.Exists(Path.Combine(Root, "logs")))
+        {
+          Directory.CreateDirectory(Path.Combine(Root, "logs"));
+        }
+
+        if (!Directory.Exists(Path.Combine(Root, "errors")))
+        {
+          Directory.CreateDirectory(Path.Combine(Root, "errors"));
+        }
+      }
+    }
+
+    public readonly static string Root = "";
+
+    private static string CustomLogging = "";
+
+    private static readonly object obj = new object();
+
+    public void AppendLog(string message, bool isWarning = false)
+    {
+      if (string.IsNullOrEmpty(CustomLogging)) return;
+      var date = DateTime.Now;
+      var info = isWarning ? "warning" : "info";
+      lock (obj)
+      {
+        File.AppendAllLines(Path.Combine(Root, "logs", $"Gateway-{date.ToString("yyyyMMdd")}.txt"), new string[]
+       {
+           $"[{date.ToString("yyyy-MM-dd HH:mm:ss zzz")}] [{info}] {message}"
+       });
+      }
+    }
+
+    public void AppendHistory(string message)
+    {
+      if (string.IsNullOrEmpty(CustomLogging)) return;
+      var date = DateTime.Now;
+      lock (obj)
+      {
+        File.AppendAllText(Path.Combine(Root, "logs", $"{date.ToString("yyyyMMdd")}.txt"), $"======\n{message}\n======\n");
+      }
+    }
+  }
+}
